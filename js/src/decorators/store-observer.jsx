34b2bcecac43175963e7f278c268ad9ada501770
Andrew Duthie <aduthie7@gmail.com>
2015-04-27 08:50:18 -0400

    Resolve tabbing inconsistencies

--- js/src/decorators/store-observer.jsx
+++ js/src/decorators/store-observer.jsx
@@ -4,30 +4,30 @@ export default function StoreObserver( Component, props ) {
 	return class extends React.Component {
 		componentDidMount() {
 			props.forEach( ( prop ) => {
-                this.props[ prop ].addListener( 'change', this.forceUpdate.bind( this ) );
-            } );
+				this.props[ prop ].addListener( 'change', this.forceUpdate.bind( this ) );
+			} );
 		}
 
 		componentWillUnmount() {
 			props.forEach( ( prop ) => {
-                this.props[ prop ].removeListener( 'change', this.forceUpdate.bind( this ) );
-            } );
+				this.props[ prop ].removeListener( 'change', this.forceUpdate.bind( this ) );
+			} );
 		}
 
 		componentDidUpdate( prevProps ) {
-            props.forEach( ( prop ) => {
-                if ( prevProps[ prop ] === this.props[ prop ] ) {
-                    return;
-                }
+			props.forEach( ( prop ) => {
+				if ( prevProps[ prop ] === this.props[ prop ] ) {
+					return;
+				}
 
-                if ( prevProps[ prop ] ) {
-                    prevProps[ prop ].removeListener( 'change', this.forceUpdate.bind( this ) );
-                }
+				if ( prevProps[ prop ] ) {
+					prevProps[ prop ].removeListener( 'change', this.forceUpdate.bind( this ) );
+				}
 
-                if ( this.props[ prop ] ) {
-                    this.props[ prop ].on( 'change', this.forceUpdate.bind( this ) );
-                }
-            } );
+				if ( this.props[ prop ] ) {
+					this.props[ prop ].on( 'change', this.forceUpdate.bind( this ) );
+				}
+			} );
 		}
 
 		render() {
